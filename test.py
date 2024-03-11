3c73fe0a4a4de06274bafe20c573992294a5255f
DuskXi <84715902+DuskXi@users.noreply.github.com>
2024-03-11 18:27:14 +0000

    build auto test ci

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TotalTest(unittest.TestCase):
+    def test_project_init(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
