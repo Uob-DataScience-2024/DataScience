6e04614c47ef1f76a4e13fa9d7df209a19e37533
DuskXi <84715902+DuskXi@users.noreply.github.com>
2024-03-14 00:35:24 +0000

    change model

--- experiment/model.py
+++ experiment/model.py
@@ -4,11 +4,11 @@ import torch.nn.functional as F
 
 
 class Seq2Seq(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, batch_first=True):
+    def __init__(self, input_dim, hidden_dim, output_dim, batch_first=True, num_layers=6, dropout=0.25):
         super(Seq2Seq, self).__init__()
         self.hidden_dim = hidden_dim
-        self.encoder = nn.LSTM(input_dim, hidden_dim, batch_first=batch_first)
-        self.decoder = nn.LSTM(hidden_dim, hidden_dim, batch_first=batch_first)
+        self.encoder = nn.LSTM(input_dim, hidden_dim, batch_first=batch_first, num_layers=num_layers, dropout=dropout)
+        self.decoder = nn.LSTM(hidden_dim, hidden_dim, batch_first=batch_first, num_layers=num_layers, dropout=dropout)
         self.linear = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
