12d24c9b88ca818c613b722d5769c7a73beca7d0
DuskXi <84715902+DuskXi@users.noreply.github.com>
2024-03-25 13:21:23 +0000

    model
    Co-authored-by: Uni_Account <95746968+dorrrrrrrr@users.noreply.github.com>

---
+++ src/model.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+
+
+class LSTMClassifier(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(LSTMClassifier, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.fc(out[:, -1, :])
+        return out
