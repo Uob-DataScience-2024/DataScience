3aa274b5c0268598e4d786f7242e9c952cd2691e
DuskXi <84715902+DuskXi@users.noreply.github.com>
2024-03-25 19:09:19 +0000

    model update
    Co-authored-by: Uni_Account <95746968+dorrrrrrrr@users.noreply.github.com>

--- src/model.py
+++ src/model.py
@@ -8,9 +8,20 @@ class LSTMClassifier(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
+        self.fc = nn.Sequential(
+            nn.Linear(hidden_size * 2 * num_layers, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+            nn.Linear(hidden_size, num_classes),
+        )
 
     def forward(self, x):
-        out, _ = self.lstm(x)
-        out = self.fc(out[:, -1, :])
+        out, hidden = self.lstm(x)
+        x = torch.concat([hidden[0], hidden[1]], dim=1)
+        x = x.view(out.size(0), -1)
+        out = self.fc(x)
         return out
