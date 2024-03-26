281511f6f2c6aafa74e67611ab8742f26f3b0956
DuskXi <84715902+DuskXi@users.noreply.github.com>
2024-03-26 21:48:43 +0000

    move
    Co-authored-by: Uni_Account <95746968+dorrrrrrrr@users.noreply.github.com>

--- src/test.py
+++ src/test.py
@@ -306,23 +306,23 @@ class TestDataset(unittest.TestCase):
         logger.info(f"X: {x.shape}, Y: {y.shape}")
         self.assertEqual(True, True)
 
-    def test_sequence_dataset_split(self):
-        from dataset import SequenceDataset
-        dataset_split = SequenceDataset(data_dir, split=True)
-        dataset_no_split = SequenceDataset(data_dir, split=False)
-        logger.info(f"Split Dataset: {len(dataset_split)}, No Split Dataset: {len(dataset_no_split)}")
-        x, y = dataset_no_split[0]
-        logger.info(f"X: {x.shape}, Y: {y.shape}")
-        X1, Y1 = dataset_split[0]
-        X2, Y2 = dataset_split[1]
-        X3, Y3 = dataset_split[2]
-        X4, Y4 = dataset_split[3]
-        logger.info(f"X1: {X1.shape}, Y1: {Y1.shape}")
-        logger.info(f"X2: {X2.shape}, Y2: {Y2.shape}")
-        logger.info(f"X3: {X3.shape}, Y3: {Y3.shape}")
-        logger.info(f"X4: {X4.shape}, Y4: {Y4.shape}")
-        logger.info(f"Sum: {X1.shape[0] + X2.shape[0] + X3.shape[0] + X4.shape[0]}, No Split: {x.shape[0]}")
-        self.assertEqual(X1.shape[0] + X2.shape[0] + X3.shape[0] + X4.shape[0], x.shape[0])
+    # def test_sequence_dataset_split(self):
+    #     from dataset import SequenceDataset
+    #     dataset_split = SequenceDataset(data_dir, split=True)
+    #     dataset_no_split = SequenceDataset(data_dir, split=False)
+    #     logger.info(f"Split Dataset: {len(dataset_split)}, No Split Dataset: {len(dataset_no_split)}")
+    #     x, y = dataset_no_split[0]
+    #     logger.info(f"X: {x.shape}, Y: {y.shape}")
+    #     X1, Y1 = dataset_split[0]
+    #     X2, Y2 = dataset_split[1]
+    #     X3, Y3 = dataset_split[2]
+    #     X4, Y4 = dataset_split[3]
+    #     logger.info(f"X1: {X1.shape}, Y1: {Y1.shape}")
+    #     logger.info(f"X2: {X2.shape}, Y2: {Y2.shape}")
+    #     logger.info(f"X3: {X3.shape}, Y3: {Y3.shape}")
+    #     logger.info(f"X4: {X4.shape}, Y4: {Y4.shape}")
+    #     logger.info(f"Sum: {X1.shape[0] + X2.shape[0] + X3.shape[0] + X4.shape[0]}, No Split: {x.shape[0]}")
+    #     self.assertEqual(X1.shape[0] + X2.shape[0] + X3.shape[0] + X4.shape[0], x.shape[0])
 
     def test_sequence_dataset_custom_col(self):
         from dataset import SequenceDataset
